0e4ac8fc40707f1ce8fe501059ad48184887a41a
João Junior <joaospj.dev@gmail.com>
2020-06-28 17:12:49 -0300

    Fully functional stopwatch with start, pause and reset

---
+++ app.js
@@ -0,0 +1,46 @@
+const time = document.querySelector("#time");
+const reset = document.querySelector("#reset");
+
+let intervalId, startTime, split;
+let totalTime = 0;
+let isStarted = false;
+
+document.body.addEventListener("click", function (evt) {
+  // Do nothng if a button is clicked, in this case the reset button
+  if (evt.path[0].localName !== "button") {
+    if (!isStarted) {
+      startTime = Date.now();
+      start();
+      isStarted = true;
+      reset.hidden = true;
+    } else {
+      pause();
+
+      isStarted = false;
+      totalTime += split;
+    }
+  }
+});
+
+reset.addEventListener("click", () => {
+  totalTime = 0;
+  time.innerHTML = "00:00.000";
+});
+
+const start = () => {
+  intervalId = setInterval(() => {
+    split = Date.now() - startTime;
+    time.innerHTML = formatTime(totalTime + split);
+  }, 1);
+};
+
+const pause = () => {
+  clearInterval(intervalId);
+};
+
+const formatTime = (time) => {
+  let sec = ((time / 1000) % 60).toFixed(3);
+  let min = Math.floor(time / 60 / 1000);
+  min = min > 10 ? min : `0${min}`;
+  return sec > 10 ? `${min}:${sec}` : `${min}:0${sec}`;
+};
