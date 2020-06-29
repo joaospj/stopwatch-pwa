cc9bcb6403b5594b26487f199fe4a4105dd8238a
João Junior <joaospj.dev@gmail.com>
2020-06-29 13:14:03 -0300

    change cache assets folder 2

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 const cacheName = "my-cache-v1";
 const assets = [
-  "stopwatch-pwa/",
-  "stopwatch-pwa/index.html",
-  "stopwatch-pwa/style.css",
-  "stopwatch-pwa/app.js",
-  "stopwatch-pwa/manifest.json",
-  "stopwatch-pwa/images/icons/favicon.ico",
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./images/icons/favicon.ico",
   "https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap",
   "https://fonts.gstatic.com/s/roboto/v20/KFOkCnqEu92Fr1MmgVxIIzIXKMny.woff2",
 ];
